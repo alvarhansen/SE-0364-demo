d66086d94dbbf8e320d4c19bda6fb90937d2b9cf
Alvar Hansen <alvar@hansen.ee>
2024-09-30 17:18:01 +0300

    Add demo

---
+++ Sources/AppWithSwiftPackageName/AppDelegate.swift
@@ -0,0 +1,8 @@
+import UIKit
+import Framework1WithSwiftPackageName
+import Framework2WithSwiftPackageName
+
+@UIApplicationMain
+class ApplicationMain: UIResponder, UIApplicationDelegate {}
+
+extension Feature: FeatureAPI {}
---
+++ Sources/AppWithoutSwiftPackageName/AppDelegate.swift
@@ -0,0 +1,8 @@
+import UIKit
+import Framework1WithoutSwiftPackageName
+import Framework2WithoutSwiftPackageName
+
+@UIApplicationMain
+class ApplicationMain: UIResponder, UIApplicationDelegate {}
+
+extension Feature: FeatureAPI {}
